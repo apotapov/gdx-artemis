106961decda2d9819d1532f1144aae4deeba01d7
Andrew Potapov <apotapov@gmail.com>
2013-12-11 17:14:08 -0600

    Fixing a small bug in FastDeliveryEventSystem. Unit test added.

--- src/main/java/com/artemis/systems/event/FastDeliveryEventSystem.java
+++ src/main/java/com/artemis/systems/event/FastDeliveryEventSystem.java
@@ -72,7 +72,7 @@ public class FastDeliveryEventSystem extends BasicEventSystem {
 
             for (SystemEvent event : currentEvents.get(type)) {
                 // only add events if their id is higher than the last time we polled
-                if (!event.handled && events.contains(type.cast(event), false) && event.eventId > lastPolledEvent) {
+                if (!event.handled && !events.contains(type.cast(event), false) && event.eventId > lastPolledEvent) {
                     events.add(type.cast(event));
 
                     // update the current highest polled event
